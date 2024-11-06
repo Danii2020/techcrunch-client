f740cea05974bc2e2c8396c9c649a4ef37e6fcd7
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2024-11-06 18:56:22 -0500

    Express added

---
+++ controllers/cron.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const cron = async () => {
+    const url = "https://techcrunch-bot.onrender.com/bot";
+    const toNumber = "593994029754"
+    const data = {
+        "to_number": toNumber
+    };
+
+    try {
+        const response = await axios.post(url, data);
+        console.log("Response data:", response.data);
+        res.status(200).send("POST request successful");
+    } catch (error) {
+        console.error("Error making POST request:", error.message);
+        res.status(500).send("Error making POST request");
+    }
+}
