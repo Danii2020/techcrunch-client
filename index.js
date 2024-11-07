0ffd2aaa7949c6274caef85606661eb6e5a7c4d6
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2024-11-06 19:38:08 -0500

    Action added

---
+++ index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const sendRequest = async () => {
+    const url = "https://techcrunch-bot.onrender.com/bot";
+    const toNumber = "593994029754"
+    const data = {
+        "to_number": toNumber
+    };
+
+    try {
+        const response = await axios.post(url, data);
+        console.log("Response data:", response.data);
+    } catch (error) {
+        console.error("Error making POST request:", error.message);
+    }
+}
+
+sendRequest()
